6d75ccaeab20c915ab6babac11641d94439da9b7
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-09-22 22:23:32 +0200

    Column.tsx: add mobx store and change layout

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,20 +1,33 @@
-import classes from "./Column.module.scss";
+import { observer } from "mobx-react-lite";
 import DragIcon from "./icons/DragIcon";
+import Task from "./Task";
+import classes from "./Column.module.scss";
+import { taskInterface } from "../utils/interfaces";
 
 type ColumnType = {
   name: string;
+  tasks: taskInterface[];
+  order: number | null;
 };
 
-function Column({ name }: ColumnType) {
+const Column = observer(function Column({ name, tasks, order }: ColumnType) {
   return (
     <div className={classes.column}>
       <div className={classes.drag_container}>
         <h2>{name}</h2>
         <DragIcon />
       </div>
-      <div className={classes.tasks_container}></div>
+      <div className={classes.tasks_container}>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <Task task={task} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+});
 
 export default Column;
