49db7dbb1325651c501f84750b1c0fe09a4fea6c
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-09-22 22:25:39 +0200

    Main.tsx: add mobx store and change layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,27 @@
-import classes from "./Main.module.scss";
-import kanbanColumns from "../utils/kanbanColumns";
 import Column from "./Column";
+import { observer } from "mobx-react-lite";
+import { columns } from "../store/columnsStore";
+import classes from "./Main.module.scss";
 
-function Main() {
-  const columns = Object.entries(kanbanColumns).map(([_, value]) => value);
+const Main = observer(function Main() {
+  const columnsArray = Object.entries(columns.columns).map(
+    ([_, value]) => value
+  );
 
   return (
     <main className={classes.main}>
       <div className={classes.kanban_board}>
-        {columns.map((column) => {
-          return <Column key={column.id} name={column.name} />;
-        })}
+        {columnsArray.map((column) => (
+          <Column
+            key={column.id}
+            name={column.name}
+            tasks={column.tasks}
+            order={column.order}
+          />
+        ))}
       </div>
     </main>
   );
-}
+});
 
 export default Main;
