53d3b20b97158d7ae8ec07cc584c1209e6ae1252
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-09-22 22:27:11 +0200

    Options.tsx: add props

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,15 +1,24 @@
+import { ChangeEvent } from "react";
 import classes from "./Options.module.scss";
 
 type OptionsType = {
   id: string;
   options: readonly string[];
+  value: string;
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
 };
 
-function Options({ id, options }: OptionsType) {
+function Options({ id, options, value, onChange }: OptionsType) {
   return (
     <div className={classes.select_container}>
       <label htmlFor={id}>{id}</label>
-      <select name={id} id={id} className={classes.select}>
+      <select
+        name={id}
+        id={id}
+        className={classes.select}
+        value={value}
+        onChange={onChange}
+      >
         {options.map((option) => (
           <option key={option} value={option}>
             {option}
