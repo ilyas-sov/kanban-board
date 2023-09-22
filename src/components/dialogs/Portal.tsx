3e9de97aad34f3e1e44df9e92c960550a500b3d5
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-09-22 22:31:34 +0200

    Add Portal.tsx

---
+++ src/components/dialogs/Portal.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+import { createPortal } from "react-dom";
+import classes from "./Portal.module.scss";
+
+function DialogWindow({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <>
+      <div className={classes.backdrop}></div>
+      <div className={`${classes.dialog} ${className}`}>{children}</div>
+    </>
+  );
+}
+
+function Portal({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return createPortal(
+    <DialogWindow className={className}>{children}</DialogWindow>,
+    document.getElementById("dialog") as HTMLDivElement
+  );
+}
+
+export default Portal;
