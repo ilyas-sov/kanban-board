5bc40d5051d037ac8c95ad5d5e30c5edaac3fa49
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-09-22 13:03:26 +0200

    Header.tsx: add 'New Column' button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,15 @@ import classes from "./Header.module.scss";
 import NewTaskDialog from "./NewTaskDialog";
 
 function Header() {
-  const [dialogIsOpen, setDialogIsOpen] = useState(true);
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
   return (
     <header className={classes.header}>
       <h1>Kanban Board</h1>
-      <button onClick={() => setDialogIsOpen(true)}>New Task</button>
+      <div className={classes.actions}>
+        <button onClick={() => setDialogIsOpen(true)}>New Task</button>
+        <button onClick={() => {}}>New Column</button>
+      </div>
       {dialogIsOpen && <NewTaskDialog onClose={() => setDialogIsOpen(false)} />}
     </header>
   );
