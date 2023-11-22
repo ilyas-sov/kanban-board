7c8b8ccfd162dededbc80f1ea6c2cae86a435437
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-11-22 16:10:18 +0100

    Column.tsx: change layout

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,16 +1,25 @@
+import { Fragment } from "react";
 import { observer } from "mobx-react-lite";
+import { Columns, Task } from "../utils/types";
+import { v4 as uuidv4 } from "uuid";
 import DragIcon from "./icons/DragIcon";
-import Task from "./Task";
+import TaskCard from "./TaskCard";
+import DropArea from "./DropArea";
 import classes from "./Column.module.scss";
-import { taskInterface } from "../utils/interfaces";
 
 type ColumnType = {
   name: string;
-  tasks: taskInterface[];
-  order: number | null;
+  id: Columns;
+  tasks: Task[];
+  onDrop: (column: Columns, index: number) => void;
 };
 
-const Column = observer(function Column({ name, tasks, order }: ColumnType) {
+const Column = observer(function Column({
+  name,
+  id,
+  tasks,
+  onDrop,
+}: ColumnType) {
   return (
     <div className={classes.column}>
       <div className={classes.drag_container}>
@@ -18,13 +27,13 @@ const Column = observer(function Column({ name, tasks, order }: ColumnType) {
         <DragIcon />
       </div>
       <div className={classes.tasks_container}>
-        <ul>
-          {tasks.map((task) => (
-            <li key={task.id}>
-              <Task task={task} />
-            </li>
-          ))}
-        </ul>
+        <DropArea onDrop={() => onDrop(id, 0)} />
+        {tasks.map((task, index) => (
+          <Fragment key={uuidv4()}>
+            <TaskCard task={task} />
+            <DropArea onDrop={() => onDrop(id, index + 1)} />
+          </Fragment>
+        ))}
       </div>
     </div>
   );
