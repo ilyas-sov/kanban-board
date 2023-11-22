70333a6e8ad90890c17e77cc425ddde6cc5b26e4
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-11-22 16:19:21 +0100

    Add DropArea component

---
+++ src/components/DropArea.tsx
@@ -0,0 +1,41 @@
+import { DragEvent, useState } from "react";
+import classes from "./DropArea.module.scss";
+
+type DropAreaType = {
+  onDrop: () => void;
+};
+
+function DropArea({ onDrop }: DropAreaType) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function dragEnterHandler() {
+    setIsVisible(true);
+  }
+
+  function dragLeaveHandler() {
+    setIsVisible(false);
+  }
+
+  function dropHandler() {
+    onDrop();
+    dragLeaveHandler();
+  }
+
+  function dragOverHandler(e: DragEvent<HTMLElement>) {
+    e.preventDefault();
+  }
+
+  return (
+    <div
+      className={`${classes.drop_area} ${isVisible ? classes.visible : ""}`}
+      onDragEnter={dragEnterHandler}
+      onDragLeave={dragLeaveHandler}
+      onDrop={dropHandler}
+      onDragOver={dragOverHandler}
+    >
+      Drop here...
+    </div>
+  );
+}
+
+export default DropArea;
