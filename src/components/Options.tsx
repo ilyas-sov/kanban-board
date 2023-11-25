6a6448de6502208cadc752b0bb01f91e8076cd72
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-11-25 22:23:48 +0100

    Options.tsx: change layout and add props

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,10 +1,11 @@
 import { ChangeEvent } from "react";
+import { Option } from "../utils/types";
 import classes from "./Options.module.scss";
 
 type OptionsType = {
   id: string;
-  options: readonly string[];
-  value: string;
+  options: readonly Option[];
+  value?: string | undefined;
   onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
 };
 
@@ -13,9 +14,10 @@ function Options({ id, options, value, onChange }: OptionsType) {
     <div className={classes.select_container}>
       <label htmlFor={id}>{id}</label>
       <select name={id} id={id} value={value} onChange={onChange}>
+        {!value && <option value="">Select user</option>}
         {options.map((option) => (
-          <option key={option} value={option}>
-            {option}
+          <option key={option.id} id={option.id} value={option.value}>
+            {option.value}
           </option>
         ))}
       </select>
