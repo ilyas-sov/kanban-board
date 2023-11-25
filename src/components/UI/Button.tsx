0571216941e564023d2edb4d91f2c7ff224fb69f
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-11-25 22:39:56 +0100

    Add Button component

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,27 @@
+import { PropsWithChildren } from "react";
+import classes from "./Button.module.scss";
+
+type ButtonType = {
+  className?: string;
+  disabled?: boolean;
+  onClick: () => void;
+};
+
+function Button({
+  className,
+  disabled,
+  onClick,
+  children,
+}: PropsWithChildren<ButtonType>) {
+  return (
+    <button
+      onClick={onClick}
+      className={`${classes.button} ${className}`}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
