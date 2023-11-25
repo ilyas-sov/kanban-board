6a0c255a85324466fcb4db2c7c70f34f5c9844a9
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-11-25 22:35:03 +0100

    tasksStore.ts: add assignUser and getTask functions

--- src/store/tasksStore.ts
+++ src/store/tasksStore.ts
@@ -1,6 +1,6 @@
 import { makeAutoObservable } from "mobx";
 import { Columns, Task, Tasks } from "../utils/types";
-import { initialTasks } from "../utils/tasks";
+import { initialTasks } from "../utils/mockData/tasks";
 
 class TasksStore {
   tasks: Tasks = initialTasks;
@@ -11,6 +11,32 @@ class TasksStore {
 
   addTask(taskData: Task) {
     this.tasks[Columns.TODO].push(taskData);
+  }
+
+  assignUser(column: Columns, taskId: string, userId: string) {
+    const task = this.tasks[column].find((task) => task.id === taskId);
+    task?.users.push(userId);
+  }
+
+  getTask(id: string | undefined) {
+    if (!id) return;
+    const tasksArray = Object.entries(this.tasks);
+
+    let task;
+
+    for (let i = 0; i < tasksArray.length; i++) {
+      const currentColumn = tasksArray[i][0];
+      const currentTasks = tasksArray[i][1];
+
+      task = currentTasks.find((task) => task.id === id);
+
+      if (task) {
+        task = { ...task, status: currentColumn };
+        break;
+      }
+    }
+
+    return task;
   }
 }
 
