463986b7bca0d0b805d60a2a522f1f67479304da
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-11-25 22:21:31 +0100

    App.tsx: change layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import TaskDetails from "./components/TaskDetails/TaskDetails";
+import classes from "./App.module.css";
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <main className={classes.main}>
+        <Routes>
+          <Route path="/" index element={<Navigate to="/tasks" />} />
+          <Route path="/tasks" element={<Main />} />
+          <Route path="/tasks/:id" element={<TaskDetails />} />
+        </Routes>
+      </main>
     </>
   );
 }
