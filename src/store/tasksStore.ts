41c9e889c0a35a1322c68041514ec8414d98b28b
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-11-27 18:25:53 +0100

    tasksStore.ts: add deleteUser, changePriority and changeStatus functions

--- src/store/tasksStore.ts
+++ src/store/tasksStore.ts
@@ -15,7 +15,52 @@ class TasksStore {
 
   assignUser(column: Columns, taskId: string, userId: string) {
     const task = this.tasks[column].find((task) => task.id === taskId);
+
     task?.users.push(userId);
+  }
+
+  deleteUser(column: Columns, taskId: string, userId: string) {
+    const task = this.tasks[column].find((task) => task.id === taskId);
+
+    if (task) {
+      const taskId = this.tasks[column].indexOf(task);
+      const updatedUsers = task?.users.filter((id) => id !== userId);
+      this.tasks[column][taskId].users = updatedUsers;
+    }
+  }
+
+  changePriority(column: Columns, taskId: string, newPriority: string) {
+    const task = this.tasks[column].find((task) => task.id === taskId);
+
+    if (task) {
+      const taskId = this.tasks[column].indexOf(task);
+      this.tasks[column][taskId].priority = newPriority;
+    }
+  }
+
+  changeStatus(
+    column: Columns,
+    taskId: string,
+    newColumn: Columns,
+    index?: number
+  ) {
+    const task = this.tasks[column].find((task) => task.id === taskId);
+
+    if (task) {
+      this.tasks[column] = this.tasks[column].filter(
+        (task) => task.id !== taskId
+      );
+
+      if (index) {
+        this.tasks[newColumn] = [
+          ...this.tasks[newColumn].slice(0, index),
+          { ...task, status: newColumn },
+          ...this.tasks[newColumn].slice(index),
+        ];
+      } else {
+        this.tasks[newColumn].push({ ...task, status: newColumn });
+      }
+    }
   }
 
   getTask(id: string | undefined) {
