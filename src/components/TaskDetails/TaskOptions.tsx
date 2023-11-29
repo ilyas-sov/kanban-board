c43ac9b9b7b7887aeac6f5275af321210c1be3c5
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-11-29 18:14:26 +0100

    Add TaskOptions component

---
+++ src/components/TaskDetails/TaskOptions.tsx
@@ -0,0 +1,24 @@
+import classes from "./TaskDetails.module.scss";
+
+type TaskOptionsType = {
+  options: string[];
+  onDeleteTask: () => void;
+};
+
+function TaskOptions({ options, onDeleteTask }: TaskOptionsType) {
+  return (
+    <div className={classes.options_container}>
+      <ul>
+        {options.map((option) => (
+          <li key={option}>
+            <button className={classes.btn} onClick={onDeleteTask}>
+              {option}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TaskOptions;
