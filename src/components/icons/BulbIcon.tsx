b6365dd8891526e168841462585ec0a3df089ba7
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-11-30 17:26:07 +0100

    Add BulbIcon component

---
+++ src/components/icons/BulbIcon.tsx
@@ -0,0 +1,90 @@
+function BulbIcon({ className }: { className: string }) {
+  return (
+    <svg
+      width="29px"
+      height="29px"
+      viewBox="0 0 64 64"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <g data-name="Layer 31" id="Layer_31">
+        <path
+          fill="none"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2px"
+          d="M51.36,21.2a19.68,19.68,0,1,0-35.2,12.1,29.22,29.22,0,0,1,6.51,18v1.22h18V50.71A27.52,27.52,0,0,1,47,33.5,19.57,19.57,0,0,0,51.36,21.2Z"
+        />
+
+        <line
+          fill="none"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2px"
+          x1="22.23"
+          x2="41.13"
+          y1="55.58"
+          y2="55.58"
+        />
+
+        <line
+          fill="none"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2px"
+          x1="22.23"
+          x2="41.13"
+          y1="59.25"
+          y2="59.25"
+        />
+
+        <line
+          fill="none"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2px"
+          x1="22.23"
+          x2="41.13"
+          y1="62.92"
+          y2="62.92"
+        />
+
+        <line
+          fill="none"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2px"
+          x1="31.68"
+          x2="31.68"
+          y1="48.83"
+          y2="32"
+        />
+
+        <polyline
+          fill="none"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2px"
+          points="35.08 48.83 35.08 39.83 39.08 39.83"
+        />
+
+        <polyline
+          fill="none"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2px"
+          points="28.33 48.83 28.33 39.83 24.17 39.83"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default BulbIcon;
