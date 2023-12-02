61aaaac51c0258e9c69d8f64cbb5793cdfa73663
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-12-02 20:23:12 +0100

    Header.tsx: add navbar and add check for changed and unsaved task while redirecting to another page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,52 @@
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
+import { Link } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import { tasksStore } from "../store/tasksStore";
 import NewTaskDialog from "./dialogs/NewTaskDialog";
 import NewColumnDialog from "./dialogs/NewColumnDialog";
 import ThemeToggler from "./ThemeToggler/ThemeToggler";
 import classes from "./Header.module.scss";
 
-function Header() {
+const Header = observer(function Header() {
   const [taskDialogIsOpen, setTaskDialogIsOpen] = useState(false);
   const [columnDialogIsOpen, setColumnDialogIsOpen] = useState(false);
 
+  const taskWasChanged = tasksStore.taskWasChanged;
+
+  function goHomeHandler(e: SyntheticEvent, path: string = "/") {
+    if (taskWasChanged) {
+      e.preventDefault();
+      alert("Please save the changes or cancel them before leaving the page.");
+    }
+  }
+
   return (
     <header className={classes.header}>
-      <h1>Kanban Board</h1>
+      <h1>
+        <Link to="/" onClick={goHomeHandler}>
+          Kanban Board
+        </Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link
+              to="/tasks"
+              onClick={(e: SyntheticEvent) => goHomeHandler(e, "/tasks")}
+            >
+              Tasks
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/users"
+              onClick={(e: SyntheticEvent) => goHomeHandler(e, "/users")}
+            >
+              Users
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <div className={classes.actions}>
         <button onClick={() => setTaskDialogIsOpen(true)}>Add Task</button>
         <button onClick={() => setColumnDialogIsOpen(true)}>Add Column</button>
@@ -24,6 +60,6 @@ function Header() {
       )}
     </header>
   );
-}
+});
 
 export default Header;
