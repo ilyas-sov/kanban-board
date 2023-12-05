8215e734dd15ed5ae50e7029bf00449a6fca34a3
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-12-05 22:55:02 +0100

    mockData: updated data for tasks and users

--- src/utils/mockData/tasks.ts
+++ src/utils/mockData/tasks.ts
@@ -4,48 +4,57 @@ export const initialTasks: Tasks = {
   [Columns.TODO]: [
     {
       id: "001",
-      title:
-        "Task 1 with some very long title which is much more than 70 characters and definitely will wrap more than one time.",
-      description: "Description for Task 1",
+      created_at: 1698135600,
+      title: "Revamp Website Interface",
+      description:
+        "Overhaul the website UI/UX design to enhance user engagement and accessibility. Implement modern design principles and responsive layouts for a seamless browsing experience.",
       status: Columns.TODO,
       priority: "high",
-      users: ["1"],
+      users: ["1", "6"],
     },
     {
       id: "002",
-      title: "Task 2",
-      description: "Description for Task 2",
+      created_at: 1698135600,
+      title: "Content Optimization Strategy",
+      description:
+        "Develop a comprehensive content optimization plan to improve search engine rankings and user engagement. Conduct keyword research, update meta descriptions, and enhance content quality.",
       status: Columns.TODO,
       priority: "medium",
-      users: ["2"],
+      users: ["2", "4", "6"],
     },
   ],
   [Columns.IN_PROGRESS]: [
     {
       id: "003",
-      title: "Task 3",
-      description: "Description for Task 3",
+      created_at: 1698135600,
+      title: "Enhance Data Analytics Module",
+      description:
+        "Expand the functionality of the data analytics module by integrating advanced algorithms for predictive analysis. Improve data visualization tools and optimize data processing techniques.",
       status: Columns.IN_PROGRESS,
       priority: "low",
-      users: ["2"],
+      users: ["2", "5"],
     },
   ],
   [Columns.DONE]: [
     {
       id: "004",
-      title: "Task 4",
-      description: "Description for Task 4",
+      created_at: 1698135600,
+      title: "Bug Fixes and Performance Tuning",
+      description:
+        "Address reported bugs and issues across the application. Optimize codebase for better performance and scalability, ensuring a smoother user experience.",
       status: Columns.DONE,
       priority: "high",
-      users: ["1", "2"],
+      users: ["1", "2", "3"],
     },
     {
       id: "005",
-      title: "Task 5",
-      description: "Description for Task 5",
+      created_at: 1698135600,
+      title: "Product Launch Marketing Plan",
+      description:
+        "Develop a comprehensive marketing strategy for the upcoming product launch. Outline promotional campaigns, target audience identification, and channel selection for maximum outreach.",
       status: Columns.DONE,
       priority: "medium",
-      users: ["1"],
+      users: ["1", "4"],
     },
   ],
 };
--- src/utils/mockData/users.ts
+++ src/utils/mockData/users.ts
@@ -1,7 +1,76 @@
 import { User } from "../types";
 
 export const initialUsers: User[] = [
-  { id: "1", name: "John", surname: "Doe", tasks: ["001", "004", "005"] },
-  { id: "2", name: "Alice", surname: "Smith", tasks: ["002", "003", "004"] },
-  { id: "3", name: "Ilyas", surname: "Ilyasau", tasks: [] },
+  {
+    id: "1",
+    name: "John",
+    surname: "Doe",
+    tasks: ["001", "004", "005"],
+    role: "Backend Developer",
+    photo: null,
+    contacts: {
+      email: "john@example.com",
+      phone: "+1234567890",
+    },
+  },
+  {
+    id: "2",
+    name: "Alice",
+    surname: "Smith",
+    tasks: ["002", "003", "004"],
+    role: "Frontend Developer",
+    photo: null,
+    contacts: {
+      email: "alice@example.com",
+      phone: "+1987654321",
+    },
+  },
+  {
+    id: "3",
+    name: "Grace",
+    surname: "Williams",
+    tasks: ["004"],
+    role: "Backend Developer",
+    photo: null,
+    contacts: {
+      email: "grace@example.com",
+      phone: null,
+    },
+  },
+  {
+    id: "4",
+    name: "Emma",
+    surname: "Johnson",
+    tasks: ["002", "005"],
+    role: "Senior QA Engineer",
+    photo: null,
+    contacts: {
+      email: "emma@example.com",
+      phone: "+1122334455",
+    },
+  },
+  {
+    id: "5",
+    name: "Michael",
+    surname: "Brown",
+    tasks: ["003"],
+    role: "Frontend Developer",
+    photo: null,
+    contacts: {
+      email: "michael@example.com",
+      phone: null,
+    },
+  },
+  {
+    id: "6",
+    name: "Daniel",
+    surname: "Miller",
+    tasks: ["001", "002"],
+    role: "DevOps Engineer",
+    photo: null,
+    contacts: {
+      email: "daniel@example.com",
+      phone: "+6677889900",
+    },
+  },
 ];
