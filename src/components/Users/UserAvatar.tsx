0887a16ed152287adc80e68ea7067aa35fc3b7b6
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2023-12-08 18:53:11 +0100

    UserAcatar.tsx: add handler for broken image source

--- src/components/Users/UserAvatar.tsx
+++ src/components/Users/UserAvatar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import userProfileDefault from "../../assets/user-profile-default.png";
 import classes from "./Users.module.scss";
 
@@ -7,14 +8,21 @@ type UserAvatarType = {
 };
 
 function UserAvatar({ avatarSource, width }: UserAvatarType) {
+  const [imageError, setImageError] = useState(false);
+
+  function handleImageError() {
+    setImageError(true);
+  }
+
   return (
     <div
       className={classes.avatar_container}
       style={{ width: width, height: width }}
     >
       <img
-        src={avatarSource ? avatarSource : userProfileDefault}
+        src={!avatarSource || imageError ? userProfileDefault : avatarSource}
         alt="User avatar"
+        onError={handleImageError}
       />
     </div>
   );
