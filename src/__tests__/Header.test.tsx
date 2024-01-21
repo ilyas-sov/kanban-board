ba4805e8db4ec595eb52da5eb25805a209ac0746
Ilyas Ilyasov <ilyasau.ilyas@gmail.com>
2024-01-21 21:49:26 +0100

    Header.test.tsx: add tests for portals and alert

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -2,6 +2,7 @@ import { screen } from "@testing-library/react";
 import { render } from "../test-utils";
 import Header from "../components/Header";
 import user from "@testing-library/user-event";
+import { tasksStore } from "../store/tasksStore";
 
 describe("Header.tsx", () => {
   test("Logo should be rendered", () => {
@@ -12,7 +13,11 @@ describe("Header.tsx", () => {
     expect(logo).toBeInTheDocument();
   });
 
-  test("New Task dialog window should be opened on 'Add Task' button click", async () => {
+  test("New Task dialog window should be opened on 'Add Task' button click and closed on 'Cancel' button click", async () => {
+    const dialogContainer = document.createElement("div");
+    dialogContainer.id = "dialog";
+    document.body.appendChild(dialogContainer);
+
     user.setup();
 
     render(<Header />);
@@ -20,9 +25,51 @@ describe("Header.tsx", () => {
     const addTaskButton = screen.getByRole("button", { name: /Add Task/i });
 
     await user.click(addTaskButton);
-
     const dialogWindowHeading = screen.getByTestId("dialog");
-
     expect(dialogWindowHeading).toBeInTheDocument();
+
+    const cancelButton = screen.getByRole("button", { name: /Cancel/i });
+    await user.click(cancelButton);
+    expect(dialogWindowHeading).not.toBeInTheDocument();
+  });
+
+  test("New Task dialog window should be opened on 'Add Column' button click and closed on 'Cancel' button click", async () => {
+    const dialogContainer = document.createElement("div");
+    dialogContainer.id = "dialog";
+    document.body.appendChild(dialogContainer);
+
+    user.setup();
+
+    render(<Header />);
+
+    const addTaskButton = screen.getByRole("button", { name: /Add Column/i });
+
+    await user.click(addTaskButton);
+    const dialogWindowHeading = screen.getByTestId("dialog");
+    expect(dialogWindowHeading).toBeInTheDocument();
+
+    const cancelButton = screen.getByRole("button", { name: /Cancel/i });
+    await user.click(cancelButton);
+    expect(dialogWindowHeading).not.toBeInTheDocument();
+  });
+
+  test("Alert window should be shown if navigate with unsaved changes", async () => {
+    const store = tasksStore;
+    store.taskWasChanged = true;
+    user.setup();
+
+    const alertSpy = jest.spyOn(window, "alert");
+
+    render(<Header />);
+
+    const logo = screen.getByRole("link", { name: /Kanban Board/i });
+
+    await user.click(logo);
+
+    expect(alertSpy).toBeCalledWith(
+      "Please save the changes or cancel them before leaving the page."
+    );
+
+    alertSpy.mockRestore();
   });
 });
